32ad885f60c81c77eb4a076e9a5122f8e88d2dfa
yassinkazibwe <kazibweusama@gmail.com>
2024-11-02 18:55:19 +0300

    memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@
 from parameterized import parameterized
 import unittest
 from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 
 from utils import (
@@ -32,6 +33,64 @@ class TestAccessNestedMap (unittest.TestCase):
         with self.assertRaises(KeyError) as err:
             access_nested_map(nested_map, path)
             self.assertEqual(f"KeyError('{expected}')", repr(err.exception))
+
+# def get_json(url: str) -> Dict:
+#     """Get JSON from remote URL.
+#     """
+#     response = requests.get(url)
+#     return response.json()
+
+class TestGetJson (unittest.TestCase):
+    """Test json returns expected results"""
+    @patch('utils.requesrs.get')
+    def test_get_json(self, mock_get):
+
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ]
+
+        for test_url, test_payload in test_cases:
+
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+
+            mock_get.assert_called_once_with(test_url)
+
+            mock_get.reset_mock()
+
+class TestMemoize (unittest.TestCase):
+    """momoize"""
+    class TestClass:
+        def a_method(self):
+            return 42
+        
+
+    @memoize
+    def a_property(self):
+        return self.a_method()
+    with patch.object(
+        TestClass,
+        "a_method",
+        return_value=lambda:42,
+    ) as memo_fcn:
+        test_class = TestClass()
+        self.assertEqual(test_class.a_method(), 42)
+        self.assertEqual(test_class.a_propery(), 42)
+        memo.assert_called_once()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
             
-
-
